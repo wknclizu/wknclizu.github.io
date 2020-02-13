75d7bfcf81215ef7b326fc7fed818c5ae179c10f
wknclizu <1849891873@qq.com>
2020-02-13 10:53:46 +0800

    Site updated: 2020-02-13 10:53:41

--- js/post-details.js
+++ js/post-details.js
@@ -34,6 +34,14 @@ $(document).ready(function() {
   var html = $('html');
   var TAB_ANIMATE_DURATION = 200;
   var hasVelocity = $.isFunction(html.velocity);
+  
+  //折叠快
+  $(document).on('click', '.fold_hider', function(){
+    $('>.fold', this.parentNode).slideToggle();
+    $('>:first', this).toggleClass('open');
+  });
+  //默认情况下折叠
+  $("div.fold").css("display","none");
 
   $('.sidebar-nav li').on('click', function() {
     var item = $(this);
